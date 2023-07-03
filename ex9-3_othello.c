71a2b887308c09146f79a8237baf43bba3041bbe
harineko0 <harineko0927@gmail.com>
2023-07-03 23:24:44 +0900

    Added reversible

--- ex9-3_othello.c
+++ ex9-3_othello.c
@@ -3,13 +3,15 @@
 
 #define WHITE 0
 #define BLACK 1
+#define HORIZONTAL_MASK 0x7e7e7e7e7e7e7e7e
+#define VERTICAL_MASK 0x00FFFFFFFFFFFF00
+#define DIAGONAL_MASK 0x007e7e7e7e7e7e00
 
 typedef int Color;
 typedef unsigned long long ull;
+typedef ull (*shifter)(ull, int);
 
 // region Util
-
-typedef ull (*shifter)(ull, int);
 
 /// @brief 左シフトビット演算
 /// @param a シフトする対象
@@ -46,55 +48,52 @@ ull getBit(int x, int y) {
     return mask;
 }
 
-ull line(ull player, ull opponent, shifter shift,  ull n) {
-    ull result = ;
+/// @brief playerの石の隣にある、連続するopponentの石を求める.
+/// @example o: player, x: opponent として, 盤面が xxxo ならば 1110 を返す
+/// @param player プレイヤーの盤面
+/// @param opponent 敵の盤面
+/// @param shift シフトする関数. shiftR または shiftL
+/// @param n シフトする数
+ull line(ull player, ull opponent, shifter shift, int n) {
+    ull result = shift(player, n) & opponent;
+    result |= shift(result, n) & opponent;
+    result |= shift(result, n) & opponent;
+    result |= shift(result, n) & opponent;
+    result |= shift(result, n) & opponent;
+    result |= shift(result, n) & opponent;
+    return shift(result, n);
 }
 
-/// @brief direction の方向の合法手を返す
+/// @brief 特定の方向の合法手を返す
 /// @param player 自分の盤面
 /// @param opponent 敵の盤面
-/// @param watcher 番兵
+/// @param mask シフトするときに無視しないといけない opponent の石を除く. 番兵.
 /// @param shift シフトする数
-ull legal(ull player, ull opponent, ull watcher, int shift) {
-    opponent &= watcher;
-
-    ull tmp;
+ull partialLegal(ull player, ull opponent, ull mask, int shift) {
     ull legal;
 
-    // 左シフト
-    tmp = player << shift & opponent;
-    tmp |= tmp << shift & opponent;
-    tmp |= tmp << shift & opponent;
-    tmp |= tmp << shift & opponent;
-    tmp |= tmp << shift & opponent;
-    tmp |= tmp << shift & opponent;
-    legal = tmp << shift;
+    opponent &= mask;
 
-     // 右シフト
-    tmp = player >> shift & opponent;
-    tmp |= tmp >> shift & opponent;
-    tmp |= tmp >> shift & opponent;
-    tmp |= tmp >> shift & opponent;
-    tmp |= tmp >> shift & opponent;
-    tmp |= tmp >> shift & opponent;
-    legal |= tmp >> shift;
+    // 左右シフト
+    legal = line(player, opponent, shiftL, shift);
+    legal |= line(player, opponent, shiftR, shift);
 
     return legal;
 }
 
-/// @brief 合法手を返す
+/// @brief プレイヤーの合法手を返す
 /// @param player プレイヤーの盤面
 /// @param opponent 敵の盤面
 ull legalBoard(ull player, ull opponent) {
     ull blank = ~(player | opponent);
 
             // 水平
-    return blank & (legal(player, opponent, 0x7e7e7e7e7e7e7e7e, 1) |
+    return blank & (partialLegal(player, opponent, HORIZONTAL_MASK, 1) |
             // 垂直
-            legal(player, opponent, 0x00FFFFFFFFFFFF00, 8) |
+            partialLegal(player, opponent, VERTICAL_MASK, 8) |
             // 斜め
-            legal(player, opponent, 0x007e7e7e7e7e7e00, 7) |
-            legal(player, opponent, 0x007e7e7e7e7e7e00, 9));
+            partialLegal(player, opponent, DIAGONAL_MASK, 7) |
+                    partialLegal(player, opponent, DIAGONAL_MASK, 9));
 }
 
 /// @brief その場所に石を置けるか置かないかをboolで返す
@@ -104,6 +103,40 @@ ull legalBoard(ull player, ull opponent) {
 bool canPut(ull player, ull opponent, ull try) {
     ull legal = legalBoard(player, opponent);
     return (legal & try) != 0;
+}
+
+/// @brief 特定の方向の反転可能な石を返す
+/// @param player プレイヤーの盤面
+/// @param opponent 敵の盤面
+/// @param put 置いた石
+/// @param mask シフトするときのマスク
+/// @param shift 何個シフトするか
+ull partialReversible(ull player, ull opponent, ull put, ull mask, int shift) {
+    ull result = 0, tmp;
+
+    opponent &= mask;
+
+    tmp = line(put, opponent, shiftL, shift);
+    if ((player & shiftL(tmp, shift)) != 0) {
+        result = tmp;
+    }
+    tmp = line(put, opponent, shiftR, shift);
+    if ((player & shiftR(tmp, shift)) != 0) {
+        result |= tmp;
+    }
+
+    return result;
+}
+
+/// @brief 反転可能な石を返す
+/// @param player プレイヤーの盤面
+/// @param opponent 敵の盤面
+/// @param put 置いた石
+ull reversible(ull player, ull opponent, ull put) {
+    return partialReversible(player, opponent, put, HORIZONTAL_MASK, 1) |
+            partialReversible(player, opponent, put, VERTICAL_MASK, 8) |
+            partialReversible(player, opponent, put, DIAGONAL_MASK, 7) |
+            partialReversible(player, opponent, put, DIAGONAL_MASK, 9);
 }
 
 // endregion
@@ -172,7 +205,7 @@ int test() {
     //    00010000
     //    00000000
     //    00000000
-    expectULL("legalBoard()", legalBoard(getBit(3,   3) | getBit(4, 4), getBit(3, 4) | getBit(4, 3)), 0x80420100000);
+    expectULL("legal()", legal(getBit(3, 3) | getBit(4, 4), getBit(3, 4) | getBit(4, 3)), 0x80420100000);
 }
 
 // endregion
